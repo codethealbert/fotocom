35d902b747bcf206fc04a49d82ef77f278d9f08e
codethealbert <ing.alberto.lopez.l27@gmail.com>
2017-08-09 10:41:21 -0500

    Agregando formulario para Signin

--- server.js
+++ server.js
@@ -19,5 +19,5 @@ app.get('/singin', function(req,res) {
 app.listen(3000, function(err){
   if(err) return console.log('Hubo un error'), process.exit(1);
 
-  console.log('Platzigram escuchando en el puerto 3000');
+  console.log('FotoCom escuchando en el puerto 3000');
 })
