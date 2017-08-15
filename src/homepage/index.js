2199461056449c4ecafcfe49d0e1c12694e9a0e0
codethealbert <ing.alberto.lopez.l27@gmail.com>
2017-08-15 11:32:31 -0500

    Agregando layout de homepage

--- src/homepage/index.js
+++ src/homepage/index.js
@@ -1,6 +1,10 @@
 var page = require('page');
+var empty = require('empty-element');
+var template = require('./template.js');
+var title = require('title');
 
 page('/', function(ctx, next){
+  title('FotoCom');
   var main = document.getElementById('main-container');
-  main.innerHTML = '<a href="/signup">Signup</a>';
+  empty(main).appendChild(template);
 })
