42447c53845ba7ca6c84a6e245fa6b469c3196b5
codethealbert <ing.alberto.lopez.l27@gmail.com>
2017-08-15 19:35:01 -0500

    Agregando imagenes al homepage

--- src/homepage/index.js
+++ src/homepage/index.js
@@ -6,5 +6,27 @@ var title = require('title');
 page('/', function(ctx, next){
   title('FotoCom');
   var main = document.getElementById('main-container');
-  empty(main).appendChild(template);
+
+  var pictures = [
+    {
+      user:{
+        username: 'codethealbert',
+        avatar: 'https://cdn.pixabay.com/photo/2016/08/20/05/38/avatar-1606916_960_720.png'
+      },
+      url: 'office.jpg',
+      likes: 45,
+      liked: true
+    },
+    {
+      user:{
+        username: 'codethealbert',
+        avatar: 'https://cdn.pixabay.com/photo/2016/08/20/05/38/avatar-1606916_960_720.png'
+      },
+      url: 'office.jpg',
+      likes: 5,
+      liked: true
+    }
+  ];
+
+  empty(main).appendChild(template(pictures));
 })
